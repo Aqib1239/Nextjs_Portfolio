4c7a0ab76cbdbbdd45852b53895e9c3804e1e430
Aqib1239 <114600200+Aqib1239@users.noreply.github.com>
2024-10-15 12:31:46 +0530

    Update MobileNav.jsx

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -3,10 +3,6 @@ import React from "react";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { AlignJustify } from "lucide-react";
@@ -21,39 +17,40 @@ const MobileNav = () => {
       <SheetTrigger asChild>
         <AlignJustify className="cursor-pointer" />
       </SheetTrigger>
-      <SheetContent>
-        <div className="flex flex-col items-center justify-between h-full py-8">
+      
+      {/* Customize the SheetContent and move close button to the left */}
+      <SheetContent className="h-full top-0 right-2 w-full">
+        <div className="flex flex-col items-center justify-between h-fit pt-8">
           <div className="flex flex-col items-center gap-y-32">
-            <motion.div 
-            initial={{ opacity: 0, y: -100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 0.5 }}
-
-            className="flex"
+            <motion.div
+              initial={{ opacity: 0, y: -100 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 0.5 }}
+              className="flex"
             >
               <Logo />
             </motion.div>
 
-            <motion.div 
-            initial={{ opacity: 0, x: 100 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, delay: 0.8 }}
+            <motion.div
+              initial={{ opacity: 0, x: 100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, delay: 0.8 }}
             >
               <Nav
-                containerStyles="flex flex-col gap-y-6 items-center"
+                containerStyles="flex flex-col gap-y-4 items-center"
                 linkStyles="text-2xl"
               />
             </motion.div>
           </div>
 
-          <motion.div 
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 1.2 }}
-
-          containerStyles="flex gap-x-4" iconStyles="text-2xl"
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 1.2 }}
+            containerStyles="flex gap-x-4"
+            iconStyles="text-2xl"
           >
-            <Socials containerStyles="flex gap-x-4" iconStyles="text-2xl" />
+            <Socials containerStyles="flex gap-x-4 mt-48" iconStyles="text-2xl" />
           </motion.div>
         </div>
       </SheetContent>
