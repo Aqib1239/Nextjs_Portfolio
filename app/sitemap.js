9c365b92d7f996a25f29de0f3073202bf5412925
M Aqib <114600200+Aqib1239@users.noreply.github.com>
2025-09-15 12:55:12 +0530

    sitemap added

---
+++ app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://maqibportfolio.vercel.app/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://maqibportfolio.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://maqibportfolio.vercel.app/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://maqibportfolio.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
